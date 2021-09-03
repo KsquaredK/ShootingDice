7ae02ef0ac7483bd2d0b58a92484d1ed33514168
Kelly Kessler <ksquaredkelly@gmail.com>
2021-09-03 13:55:32 -0500

    first commit

---
+++ Player.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace ShootingDice
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int DiceSize { get; set; } = 6;  // (!!! note syntax)
+
+        // note virtual, i.e. can be overridden (LargeDice?)
+        public virtual int Roll()
+        {
+            // Return a random number between 1 and DiceSize
+            return new Random().Next(DiceSize) + 1;
+        }
+
+        // ?? syntax of parameter. When called in program, it takes this argument: player2.Play(player1);
+        public virtual void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShootingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating players...why () after Player?
+            Player player1 = new Player();
+            player1.Name = "Bob";
+
+            Player player2 = new Player();
+            player2.Name = "Sue";
+
+            // calling Play method from Player class
+            // ?? I don't understand syntax
+            player2.Play(player1);
+
+            Console.WriteLine("-------------------");
+
+            Player player3 = new Player();
+            player3.Name = "Wilma";
+
+            player3.Play(player2);
+
+            Console.WriteLine("-------------------");
+
+            // instantiate player from class with a property of bigger dice
+            Player large = new LargeDicePlayer();
+            large.Name = "Bigun Rollsalot";
+
+            player1.Play(large);
+
+            Console.WriteLine("-------------------");
+
+            // collection of instantiated players
+            List<Player> players = new List<Player>() {
+                player1, player2, player3, large
+            };
+            // calling method defined below, passing in collection from above
+            PlayMany(players);
+        }
+
+        static void PlayMany(List<Player> players)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Let's play a bunch of times, shall we?");
+
+            // We "order" the players by a random number
+            // This has the effect of shuffling them randomly
+            Random randomNumberGenerator = new Random();
+            List<Player> shuffledPlayers = players.OrderBy(p => randomNumberGenerator.Next()).ToList();
+
+            // We are going to match players against each other
+            // This means we need an even number of players
+            int maxIndex = shuffledPlayers.Count;
+            // ?? would this mean that if the last player in the list had an odd index, they would not be played?
+            if (maxIndex % 2 != 0)
+            {
+                maxIndex = maxIndex - 1;
+            }
+
+            // Loop over the players 2 at a time    (!!!!!!)
+            for (int i = 0; i < maxIndex; i += 2)
+            {
+                Console.WriteLine("-------------------");
+
+                // Make adjacent players play one another
+                Player player1 = shuffledPlayers[i];
+                Player player2 = shuffledPlayers[i + 1];
+                player1.Play(player2);
+            }
+        }
+    }
+}
